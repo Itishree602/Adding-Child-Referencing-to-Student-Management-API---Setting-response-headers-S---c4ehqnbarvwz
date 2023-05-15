d9de647f96914484de0e61dcbc374982bcdd2d3c
Itishree602 <122922402+Itishree602@users.noreply.github.com>
2023-05-15 14:28:04 +0530

    Update Model.js

--- models/Model.js
+++ models/Model.js
@@ -2,34 +2,54 @@
 const mongoose = require('mongoose');
 
 // Subject Schema
+
 const subjectSchema = new mongoose.Schema({
+
     name: {
+
         type: String,
+
         required: true,
+
         unique: true
+
     },
+
     subjectCode: {
+
         type: String,
+
         required: true,
+
         unique: true
+
     }
+
 });
 
-// Student Schema [Add Child Referencing Here]
-// You can create a test route that appends subject ids to subjects array of the student document, this will help you understand how child referencing works.
+// Student Schema
+
 const studentSchema = new mongoose.Schema({
+
     name: {
+
         type: String,
+
         required: true
+
     },
+
     roll: {
+
         type: String,
+
         required: true,
+
         unique: true
+
     },
-    //Add Child Referencing For Subjects Here. 
-    // subjects: []
-     subjects: [{
+
+    subjects: [{
 
         type: mongoose.Schema.Types.ObjectId,
 
@@ -40,8 +60,11 @@ const studentSchema = new mongoose.Schema({
 });
 
 // Create the models
+
 const Subject = mongoose.model('Subject', subjectSchema);
+
 const Student = mongoose.model('Student', studentSchema);
 
 // Export the models
+
 module.exports = { Subject, Student };
